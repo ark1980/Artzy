c59343a016e3b397d1b35d73c0225290919904e2
Ali Keshanian <ar.k@outlook.com>
2023-12-03 15:45:14 -0800

    add error handeling to products route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -5,10 +5,16 @@ from app.models import Product
 
 product_routes = Blueprint('products', __name__)
 
-
 @product_routes.route('/')
 def get_all_products():
+    """
+    Query for all products and returns them in a list of products dictionaries
+    """
     products_query = Product.query.all()
+
+    if not products_query:
+        return jsonify({'message': 'No products found'}), 404
+    
     products_list = [
         {
             "id": product.id,
@@ -21,6 +27,4 @@ def get_all_products():
             "quantity_available": int(product.quantity_available),
         } for product in products_query
     ]
-    return jsonify(products_list)
-
     return jsonify(products_list)
