fcb4d50e22f7b82d6e0ed7d6662cb3d11b989166
Ali Keshanian <ar.k@outlook.com>
2023-12-03 14:50:52 -0800

    get all products route completed

---
+++ app/api/product_routes.py
@@ -0,0 +1,26 @@
+from os import error
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Product
+
+product_routes = Blueprint('products', __name__)
+
+
+@product_routes.route('/')
+def get_all_products():
+    products_query = Product.query.all()
+    products_list = [
+        {
+            "id": product.id,
+            "owner_id": product.owner_id,
+            "category_id": product.category_id,
+            "name": product.name,
+            "price": str(product.price),
+            "description": product.description,
+            "category": product.category,
+            "quantity_available": str(product.quantity_available),
+        } for product in products_query
+    ]
+    return jsonify(products_list)
+
+    return jsonify(products_list)
