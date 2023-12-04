6f05a600871821a9113ba41c73b3d0d490d3b47d
Ali Keshanian <ar.k@outlook.com>
2023-12-04 10:13:27 -0800

    create New Product route completed

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,12 +1,12 @@
 from os import error
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import Product
-from app.models import User
-from flask_login import current_user, login_user, logout_user, login_required
+from flask import Blueprint, jsonify, request
+from app.models import db, Product, Category
+from flask_login import current_user
+from app.forms import CreateProduct
 
 
 product_routes = Blueprint('products', __name__)
+
 
 def to_dict_product(product):
     """
@@ -33,7 +33,7 @@ def get_all_products():
 
     if not products_query:
         return jsonify({'message': 'No products found'}), 404
-    
+
     products_list = [to_dict_product(product) for product in products_query]
 
     return jsonify(products_list)
@@ -44,16 +44,53 @@ def get_product(productId):
     query_product = Product.query.get(productId)
 
     if not query_product:
-        return jsonify({'message': 'product not found'}), 404
-    
+        return jsonify({"message": "Couldn't find a Product with the specified id"}), 404
+
     return jsonify(to_dict_product(query_product))
 
-    
+
 @product_routes.route("/users/<int:userId>")
 def get_product_by_user_id(userId):
-    if current_user.is_authenticated and current_user.id == userId:
+    if current_user.id == userId:
         user_products = Product.query.filter(Product.owner_id == userId).all()
         products = [to_dict_product(product) for product in user_products]
         return jsonify(products)
-    
-    return jsonify({'errors': ['Unauthorized']})
+
+    return jsonify({'errors': 'Not authorized user, must be the owner to see your products'}), 401
+
+
+# Create new product
+@product_routes.route('/add_product', methods=["POST"])
+def create_new_product():
+
+    form = CreateProduct()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    """
+        uncomment the below line and fix indentation for front-end
+    """
+    # if form.validate_on_submit():
+    data = form.data
+
+    if not data:
+        return jsonify({'message': 'No input data provided'}), 400
+
+    category = Category.query.filter(
+        Category.name == data['category']).first()
+
+    new_product = Product(
+        owner_id=current_user.id,
+        category_id=category.id,
+        name=data['name'],
+        price=data['price'],
+        description=data['description'],
+        category=data['category'],
+        quantity_available=data['quantity_available']
+    )
+
+    print("++++++++++==========++++++++++++=============", new_product)
+
+    # Add new product to the database
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({'message': 'Product created successfully', 'product_id': new_product.id}), 201
