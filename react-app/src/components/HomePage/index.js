ea61d903a1e4bce6f75ebd37e9ed980abba97ada
Ali Keshanian <ar.k@outlook.com>
2023-12-17 12:31:57 -0800

    Update background and font colors for improved UI contrast

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -9,6 +9,7 @@ const HomePage = () => {
     <main className="home-page">
       <div className="homepage-content">
         <h1>Welcome to Artzy</h1>
+        <h2>Find the perfect gift for your loved ones!</h2>
         <h3>Wrap up the gifting season!</h3>
       </div>
     </main>
